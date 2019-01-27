d1553d0c7c9b29aa6829e0abbe67475212cfe2d1
zackwolf <azhar.zakiy@gmail.com>
2019-01-27 17:31:23 +0800

    add verifyLogin func

--- src/app/rest-api.service.ts
+++ src/app/rest-api.service.ts
@@ -1,10 +1,31 @@
 import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {reject} from 'q';
 
 @Injectable({
     providedIn: 'root'
 })
 export class RestApiService {
 
-    constructor() {
+    apiUrl = 'http://localhost:5000/';
+
+    constructor(public http: HttpClient) {
+        console.log('RestServiceProvider Provider');
+    }
+
+    verifyLogin(data) {
+        return new Promise(resolve => {
+            this.http.post(this.apiUrl + '/verifyLogin', JSON.stringify(data), {
+                params: new HttpParams()
+                    .set('sysuser_nama', 'zackwolf')
+                    .set('sysuser_passw', 'ddc0f70da69b7e17c3b2d52a942bd72f0914d55b')
+                ,
+            })
+                .subscribe(res => {
+                    resolve(res);
+                }, (err) => {
+                    reject(err);
+                });
+        });
     }
 }
