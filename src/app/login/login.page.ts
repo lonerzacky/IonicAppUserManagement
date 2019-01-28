e9cbe66290bd30e00282a005908a0efd813ebce6
zackwolf <azhar.zakiy@gmail.com>
2019-01-29 01:32:37 +0800

    Add Event Login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+
 
 @Component({
     selector: 'app-login',
@@ -6,5 +8,28 @@ import {Component} from '@angular/core';
     styleUrls: ['login.page.scss'],
 })
 export class LoginPage {
+    constructor(public http: HttpClient) {
+    }
 
+    sysuser_nama = '';
+    sysuser_passw = '';
+
+    verifyLogin() {
+        const body = new HttpParams()
+            .set('sysuser_nama', this.sysuser_nama)
+            .set('sysuser_passw', this.sysuser_passw);
+
+        return this.http.post('http://localhost:5000/verifyLogin',
+            body.toString(),
+            {
+                headers: new HttpHeaders()
+                    .set('Content-Type', 'application/x-www-form-urlencoded')
+            }
+        ).subscribe(
+            (res) => {
+                console.log(res);
+            },
+            err => console.log(err)
+        );
+    }
 }
