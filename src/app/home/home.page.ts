706e26edc00a7f38a292a4e2834ba1f2e1f341b3
zackwolf <azhar.zakiy@gmail.com>
2019-01-29 19:04:31 +0800

    Add Logout and Information user

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Storage} from '@ionic/storage';
+import {NavController} from '@ionic/angular';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+    public sysuser_nama = '';
+
+    constructor(private storage: Storage, private navCtrl: NavController) {
+        storage.get('loginInfo').then((val) => {
+            this.sysuser_nama = val[0].sysuser_nama;
+        }).catch(function () {
+            return navCtrl.navigateBack('/login');
+        });
+    }
+
+    logout() {
+        this.storage.remove('loginInfo').then(() => {
+            return this.navCtrl.navigateBack('/login');
+        });
+    }
+
 
 }
