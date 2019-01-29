b4b17db03fbda539920d7026d71507938311958f
zackwolf <azhar.zakiy@gmail.com>
2019-01-29 23:20:44 +0800

    Add Confirm Dialog Before Logout

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {Storage} from '@ionic/storage';
 import {NavController} from '@ionic/angular';
+import {AlertController} from '@ionic/angular';
 
 @Component({
     selector: 'app-home',
@@ -9,22 +10,43 @@ import {NavController} from '@ionic/angular';
 })
 export class HomePage {
     public sysuser_nama = '';
+    public sysuser_email = '';
 
-    constructor(private storage: Storage, private navCtrl: NavController) {
+    constructor(
+        private storage: Storage,
+        private navCtrl: NavController,
+        private alertController: AlertController
+    ) {
         storage.get('loginInfo').then((val) => {
             if (val == null) {
                 return this.navCtrl.navigateBack('/login', {skipLocationChange: true});
             } else {
                 this.sysuser_nama = val[0].sysuser_nama;
+                this.sysuser_email = val[0].sysuser_email;
             }
         });
     }
 
-    logout() {
-        this.storage.remove('loginInfo').then(() => {
-            return this.navCtrl.navigateBack('/login', {skipLocationChange: true});
+    async logoutConfirm() {
+        const alert = await this.alertController.create({
+            header: 'Konfirmasi',
+            message: 'Apakah Anda Akan Logout?',
+            buttons: [
+                {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    cssClass: 'secondary'
+                }, {
+                    text: 'Logout',
+                    handler: () => {
+                        this.storage.remove('loginInfo').then(() => {
+                            return this.navCtrl.navigateBack('/login', {skipLocationChange: true});
+                        });
+                    }
+                }
+            ]
         });
+
+        await alert.present();
     }
-
-
 }
