9a2695a737c9691daa4623b2570f28a84c5d5d82
zackwolf <azhar.zakiy@gmail.com>
2019-02-04 15:43:10 +0700

    Add Confirm Before Exit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,64 @@
-import { Component } from '@angular/core';
-
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {Component, ViewChild} from '@angular/core';
+import {Router} from '@angular/router';
+import {IonRouterOutlet, Platform, AlertController} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html'
+    selector: 'app-root',
+    templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
-    this.initializeApp();
-  }
+    @ViewChild(IonRouterOutlet) routerOutlet: IonRouterOutlet;
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
+    constructor(
+        private router: Router,
+        private alertCtrl: AlertController,
+        private platform: Platform,
+        private splashScreen: SplashScreen,
+        private statusBar: StatusBar
+    ) {
+        this.initializeApp();
+        this.platform.backButton.subscribe(() => {
+            if (this.routerOutlet && this.routerOutlet.canGoBack()) {
+                this.routerOutlet.pop().then(function () {
+                });
+            } else if (this.router.url === '/home') {
+                navigator['app'].exitApp();
+            } else {
+                this.presentAlertConfirm().then(function () {
+                    console.log('Present Alert');
+                });
+            }
+        });
+    }
+
+    async presentAlertConfirm() {
+        const alert = await this.alertCtrl.create({
+            header: 'Kofirmasi!',
+            message: 'Apakah Anda ingin keluar dari aplikasi?',
+            buttons: [
+                {
+                    text: 'Cancel',
+                    handler: () => {
+                        console.log('Confirm Cancel');
+                    }
+                }, {
+                    text: 'Okay',
+                    handler: () => {
+                        navigator['app'].exitApp();
+                    }
+                }
+            ]
+        });
+
+        await alert.present();
+    }
+
+    initializeApp() {
+        this.platform.ready().then(() => {
+            this.statusBar.styleDefault();
+            this.splashScreen.hide();
+        });
+    }
 }
